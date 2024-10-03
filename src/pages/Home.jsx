7654114f4b60df8d2c2ede768f659492e63f3003
yeonhee <yeonhee12134@gmail.com>
2024-10-03 16:41:11 +0900

    favicon추가 및 배포준비

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Button from "../components/Button";
 import DiaryList from "../components/DiaryList";
 import { useState, useContext } from "react";
 import { DiaryStateContext } from "../App";
+import usePageTitle from "../hooks/usePageTitle";
 
 const getMonthlyData = (pivotDate, data) => {
   const beginTime = new Date(
@@ -29,6 +30,7 @@ const getMonthlyData = (pivotDate, data) => {
 };
 
 const Home = () => {
+  usePageTitle("감정 일기장");
   const data = useContext(DiaryStateContext);
   const [pivotDate, setPivotDate] = useState(new Date());
 
